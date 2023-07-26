881d86eef6c2f81332dc0daa0bb25d63c772c34c
mariogmazza <mariomazza01@gmail.com>
2023-07-26 15:40:23 -0400

    registration and login working

--- server/handlers/auth.js
+++ server/handlers/auth.js
@@ -3,15 +3,27 @@ const db = require('../models');
 exports.register = async (req, res, next) => {
   try {
     const user = await db.User.create(req.body);
-
-    res.json(user);
+    const { id, username } = user;
+    res.json({ id, username });
   } catch (err) {
     next(err);
   }
 };
 
-exports.login = (req, res, next) => {
+exports.login = async (req, res, next) => {
   try {
+    const user = await db.User.findOne({ username: req.body.username });
+    if (user) {
+      const { id, username } = user;
+      const valid = await user.comparePassword(req.body.password);
+      if (valid) {
+        res.json({ id, username });
+      } else {
+        throw new Error('Invalid Username/Password');
+      }
+    } else {
+      throw new Error('Invalid Username/Password');
+    }
   } catch (err) {
     next(err);
   }
