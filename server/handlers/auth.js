fbf1736a77f79af7ae6d8e93637dd8c109fdf56b
mariogmazza <mariomazza01@gmail.com>
2023-07-27 11:05:48 -0400

    middleware added

--- server/handlers/auth.js
+++ server/handlers/auth.js
@@ -1,10 +1,13 @@
+const jwt = require('jsonwebtoken');
 const db = require('../models');
 
 exports.register = async (req, res, next) => {
   try {
     const user = await db.User.create(req.body);
     const { id, username } = user;
-    res.status(201).json({ id, username });
+    const token = jwt.sign({ id, username }, process.env.SECRET);
+
+    res.status(201).json({ id, username, token });
   } catch (err) {
     if (err.code === 11000) {
       err.message = 'Sorry, that username is already taken';
@@ -20,12 +23,13 @@ exports.login = async (req, res, next) => {
       const { id, username } = user;
       const valid = await user.comparePassword(req.body.password);
       if (valid) {
-        res.json({ id, username });
+        const token = jwt.sign({ id, username }, process.env.SECRET);
+        res.json({ id, username, token });
       } else {
-        throw new Error('Invalid Username/Password');
+        throw new Error();
       }
     } else {
-      throw new Error('Invalid Username/Password');
+      throw new Error();
     }
   } catch (err) {
     err.message = 'Invalid Username/Password';
