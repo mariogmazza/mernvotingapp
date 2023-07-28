3879e13e8150fbb0e0615e34c1c4119a54735dd1
mariogmazza <mariomazza01@gmail.com>
2023-07-28 13:21:55 -0400

    fontend api routes

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # mernvotingapp
+
+### start.sh script requires permissions after creation: in the root folder where the file lives in a command line write the following "chmod +x start.sh" and after that you can simply run the bash script as "./start.sh"
--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,6 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import api from '../services/api';
 
 const App = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const results = api.call('post', 'auth/login', {
+      username: 'username',
+      password: 'password',
+    });
+
+    console.log(results);
+  }, []);
+
   return <div>App workd</div>;
 };
 
